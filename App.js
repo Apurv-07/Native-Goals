e4855b243e1c95b0765a427df2c77d4e4e3c289f
Apurv-07 <mailapurva007@gmail.com>
2022-12-17 00:27:04 +0530

    initial changes

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  var [text, setText]=useState('');
+  var [goals, setGoals]=useState([])
+  function handleClick() {
+    if(text){
+      setGoals([...goals, text])
+    }
+    console.log(goals)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.addgoal}>
+        <TextInput placeholder='Add goals' style={styles.text} onChangeText={(text)=>{
+          setText(text)
+        }}></TextInput>
+        <Button title='Add!' onPress={handleClick}/>
+      </View>
+      <View style={styles.goals}>
+        <Text>List of Goals</Text>
+        {
+
+        }
+      </View>
     </View>
   );
 }
@@ -13,8 +31,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  addgoal:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  text:{
+    borderWidth: 2,
+    marginRight: 5,
+    paddingLeft: 10,
+    width: '70%',
+    borderColor: 'grey',
+    backgroundColor: 'whitesmoke',
+  },
+  goals:{
+    flex: 4,
+  }
 });
